99ee41490b7c1590c9cf1665fa9cc20d2c107146
unknown <Mikhnov-Vaitenko.V@.egenerator.local>
2011-02-14 12:40:10 +0300

    Initial

---
+++ cfg.lua
@@ -0,0 +1,25 @@
+  local addon, ns = ...
+  local cfg = CreateFrame("Frame")
+
+  -----------------------------
+  -- MEDIA
+  -----------------------------
+  local MediaPath = "Interface\\Addons\\m_Buffs\\media\\"
+  cfg.auratex = MediaPath.."iconborder" 
+  cfg.font = MediaPath.."font.ttf"
+  cfg.backdrop_texture = MediaPath.."backdrop"
+  cfg.backdrop_edge_texture = MediaPath.."backdrop_edge"
+  
+  -----------------------------
+  -- CONFIG
+  -----------------------------
+  cfg.iconsize = 37 									-- Buffs and debuffs size
+  cfg.timefontsize = 14									-- Time font size
+  cfg.countfontsize = 15								-- Count font size
+  cfg.spacing = 4										-- Spacing between icons
+  cfg.timeYoffset = -3									-- Verticall offset value for time text field
+  cfg.BUFFpos = {"TOPRIGHT","UIParent", -25, -25} 		-- Buffs position
+  cfg.DEBUFFpos = {"TOPRIGHT", "UIParent", -25, -120}	-- Debuffs position
+
+  -- HANDOVER
+  ns.cfg = cfg
